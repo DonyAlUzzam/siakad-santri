4c348d060e3888bdf609ec941d26a196bd212f44
M Khairi usman <mkhairiusman@gmail.com>
2021-12-23 14:23:21 +0700

    route home

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    if(Auth::check()) {
+        return redirect('/dashboard');
+    } else {
+        return view('auth.login');
+    }
 });
 
 Auth::routes();
